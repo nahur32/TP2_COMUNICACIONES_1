769886be1f22fb1849958cec2c55785ec23fc64a
AgCap <agustincappiello@gmail.com>
2025-09-28 23:55:49 -0300

    Actividad 4
    actividad 4 terminada

--- codigos/ejercicio_4.py
+++ codigos/ejercicio_4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# a) Señal de amplitud aleatoria uniforme, distribuida entre [3 17]
+# a) Señal de amplitud aleatoria uniforme, distribuida entre [8 40]
 t = np.arange(0, 1, 0.0001)
 A = np.random.uniform(8, 40, size=len(t))
 x1 = A * np.cos(2 * np.pi * 10 * t)
@@ -14,6 +14,7 @@ plt.title('Amplitud aleatoria')
 C1 = np.correlate(x1, x1, mode='full')
 plt.subplot(2, 1, 2)
 plt.plot(C1)
+plt.savefig('imagenes/Actividad_4/senal_a.png')
 plt.show()
 
 # b) Señal de fase aleatoria uniforme, distribuida entre [-0.8π, 0.8π]
@@ -28,10 +29,11 @@ plt.title('Fase aleatoria')
 C2 = np.correlate(x2, x2, mode='full')
 plt.subplot(2, 1, 2)
 plt.plot(C2)
+plt.savefig('imagenes/Actividad_4/senal_b.png')
 plt.show()
 
-# c) Señal de frecuencia aleatoria uniforme, distribuida entre [3 20]
-f = np.random.uniform(3, 20, size=len(t))
+# c) Señal de frecuencia aleatoria uniforme, distribuida entre [0 20]
+f = np.random.uniform(0, 20, size=len(t))
 x3 = A2 * np.cos(2 * np.pi * f * t)
 
 plt.figure()
@@ -41,6 +43,7 @@ plt.title('Frecuencia aleatoria')
 C3 = np.correlate(x3, x3, mode='full')
 plt.subplot(2, 1, 2)
 plt.plot(C3)
+plt.savefig('imagenes/Actividad_4/senal_c.png')
 plt.show()
 
 # d) Señal de amplitud, fase y frecuencia aleatoria
@@ -53,16 +56,5 @@ plt.title('Amplitud, Fase y Frecuencia aleatoria')
 C4 = np.correlate(x4, x4, mode='full')
 plt.subplot(2, 1, 2)
 plt.plot(C4)
-plt.show()
-
-# e) Señal periódica
-x5 = 5 * np.cos(2 * np.pi * 10 * t)
-
-plt.figure()
-plt.subplot(2, 1, 1)
-plt.plot(t, x5)
-plt.title('Amplitud, Fase y Frecuencia estable')
-C5 = np.correlate(x5, x5, mode='full')
-plt.subplot(2, 1, 2)
-plt.plot(C5)
+plt.savefig('imagenes/Actividad_4/senal_d.png')
 plt.show()
